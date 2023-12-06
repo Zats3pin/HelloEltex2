5ea2f3ded4c75a8735d18c2526850b55287effd0
Zats3pin <zck2002@mail.ru>
2023-12-06 19:56:48 +0300

    работает1234567(отступы)

--- app/src/main/java/com/eltex/androidschool/MainActivity.kt
+++ app/src/main/java/com/eltex/androidschool/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.eltex.androidschool.adapter.EventsAdapter
+import com.eltex.androidschool.adapter.OffsetDecoration
 
 import com.eltex.androidschool.databinding.ActivityMainBinding
 import com.eltex.androidschool.databinding.CardEventBinding
@@ -41,6 +42,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.root.adapter = adapter
+
+        binding.root.addItemDecoration(OffsetDecoration(resources.getDimensionPixelSize(R.dimen.small_spacing)))
 
         viewModel.state
             .flowWithLifecycle(lifecycle)
---
+++ app/src/main/java/com/eltex/androidschool/adapter/OffsetDecoration.kt
@@ -0,0 +1,30 @@
+package com.eltex.androidschool.adapter
+
+import android.graphics.Rect
+import android.view.View
+import  androidx.annotation.Px
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
+
+class OffsetDecoration(
+    @Px private val offset: Int
+) : ItemDecoration(){
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        outRect.top += offset
+        outRect.left += offset
+        outRect.right += offset
+       val lastIndex = parent.adapter?.itemCount?.minus(1)
+        val lastViewElement = (parent.layoutManager as LinearLayoutManager).findLastVisibleItemPosition()
+
+        if (lastIndex == lastViewElement){
+            outRect.bottom += offset
+        }
+    }
+}
