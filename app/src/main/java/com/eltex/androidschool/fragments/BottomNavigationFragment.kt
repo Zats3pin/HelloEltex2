265d4a10354e3ea7c6992ab22453056b7767c911
Zats3pin <zck2002@mail.ru>
2023-12-22 15:07:15 +0300

    22 dz Миграция Activity -> Fragment[] граф навигации сделан нужен тест

--- app/src/main/java/com/eltex/androidschool/fragments/BottomNavigationFragment.kt
+++ app/src/main/java/com/eltex/androidschool/fragments/BottomNavigationFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.eltex.androidschool.R
@@ -20,12 +22,48 @@ class BottomNavigationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        val  binding = FragmentBottomNavigationBinding.inflate(inflater,container,false)
+        val binding = FragmentBottomNavigationBinding.inflate(inflater, container, false)
 
-        val navController =requireNotNull(childFragmentManager.findFragmentById(R.id.container)).findNavController()
+        val navController =
+            requireNotNull(childFragmentManager.findFragmentById(R.id.container)).findNavController()
         binding.bottomNavigation.setupWithNavController(navController)
+
+
+        val newEventListener = View.OnClickListener {
+        findNavController().navigate(R.id.action_bottomNavigationFragment_to_newPostFragment)
+        }
+        val newPostListener = View.OnClickListener {
+           //TODO
+        }
+        val newUserListener = View.OnClickListener {
+            //TODO
+        }
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+        when(destination.id){
+            R.id.eventsFragment ->{
+                binding.newPost.setOnClickListener { newEventListener }
+                binding.newPost.animate()
+                    .scaleX(1F)
+                    .scaleY(1F)
+            }
+            R.id.postsFragment ->{
+                binding.newPost.setOnClickListener {newPostListener}
+                binding.newPost.animate()
+                    .scaleX(1F)
+                    .scaleY(1F)
+            }
+            R.id.usersFragment ->{
+                binding.newPost.setOnClickListener {null}
+                binding.newPost.animate()
+                    .scaleX(0F)
+                    .scaleY(0F)
+            }
+
+
+        }
+        }
+
         return binding.root
     }
-
-
 }
