f65d4518644a179b830832d05b9efc574ce7f716
Zats3pin <zck2002@mail.ru>
2024-01-12 20:43:51 +0300

    24 dz
    c сервера не могу читать посты, хотя запрос доходит и возвращает нужное
    скорее всего связано с потерей кадров
    с сервера считываются посты[]
    работают лайки[]
    работают евенты[]
    работает создание[V]
    работает удаление[]
    работает редактирование[]
    код красивый[]

--- app/src/main/java/com/eltex/androidschool/utils/ThrowableExtensions.kt
+++ app/src/main/java/com/eltex/androidschool/utils/ThrowableExtensions.kt
@@ -2,8 +2,14 @@ package com.eltex.androidschool.utils
 
 import android.content.Context
 import com.eltex.androidschool.R
+import java.io.FileNotFoundException
 import java.io.IOException
+import java.net.ConnectException
+import java.net.SocketTimeoutException
+import javax.net.ssl.SSLHandshakeException
+
 fun Throwable.getText(context: Context): String = when (this) {
        is IOException -> context.getString(R.string.network_error)
         else -> context.getString(R.string.unknown_error)
+
 }
